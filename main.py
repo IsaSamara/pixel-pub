10af4bab2768519b3ed87380a33ba78132ced97e
Diego Silva <diegopereirasilva96@gmail.com>
2025-10-16 22:37:50 -0300

    Adicionado para o Swagger abri automaticamente

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # uvicorn main:app --reload
+import webbrowser
 from fastapi import FastAPI
 from routers import produto
 
 app = FastAPI()
 
 app.include_router(produto.router)
+
+import threading
+def open_browser():
+    webbrowser.open("http://127.0.0.1:8000/docs")
+
+threading.Timer(1.5, open_browser).start()
